b977e293515b06042b3f1bed56254cfabdebedc1
Marcus Rettig <marcus0706@live.se>
2018-05-08 15:44:12 +0200

    similarityScore

--- src/StringAlignment.hs
+++ src/StringAlignment.hs
@@ -1,9 +1,26 @@
 module StringAlignment where
+import Data.List
+import Data.Char
 
 scoreMatch = 0
 scoreMismatch = -1
 scoreSpace = -1
 
+score :: Char -> Char -> Int
+score '-' y = scoreSpace
+score x '-' = scoreSpace
+score x y
+  | x == y = scoreMatch
+  | otherwise = scoreMismatch
+
+
 similarityScore :: String -> String -> Int
-similarityScore s1 s2 = 1
+similarityScore [] [] = 0
+similarityScore s [] = (*) scoreMismatch $ length s
+similarityScore [] s = (*) scoreMismatch $ length s
+similarityScore (x:xs) (y:ys) =
+  maximum
+  [ (similarityScore xs     ys    ) + (score x   y)
+  , (similarityScore (x:xs) ys    ) + (score '-' y)
+  , (similarityScore xs     (y:ys)) + (score x '-') ]
 
--- test/StringAlignmentTest.hs
+++ test/StringAlignmentTest.hs
@@ -4,9 +4,11 @@ module Main where
   import Test.HUnit
 
   similarityScoreTest =
-    test [
-      similarityScore "writers" "vintner" ~?= -5
-    ]
+    test
+    [ similarityScore "writers" "vintner" ~?= -5
+    , similarityScore "a" "" ~?= (1 * scoreMismatch)
+    , similarityScore "" "aa" ~?= (2 * scoreMismatch)
+    , similarityScore "" "" ~?= 0 ]
 
   main = runTestTT $
     test [
